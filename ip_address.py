07d30dbf53001459e7a57100e7ad6d336e59478b
nyamongocliff/mycode <nyamongc5341@student.faytechcc.edu>
2024-07-11 23:47:16 +0000

    Ip address modification

--- ip_address.py
+++ ip_address.py
@@ -23,12 +23,12 @@ def process_ip_network(user_input):
             print(f"Usable IP: {first_host} - {last_host}")
         else:
             print(f"Single host: {hosts[0]}")
-
+        return True           #Indicate success.
 
     except ValueError as e:
         #  Handle exceptions if the input is not a valid IP network
         print(f"Error: {e}")
-        
+    return False              #Indicate failure
 
 #Get the IP class
 def get_ip_class(user_input):
@@ -49,19 +49,32 @@ def get_ip_class(user_input):
 
 
     ip_class = get_ip_class(user_input)
-    print(f"Class: {ip_class}")
     
-
 def main():
     while True:
-        user_input = input("Enter an IP address and network prefix (e.g., '192.168.1.0/24')or 'Exit' to quit: ").strip().lower()   #  Allow user input
+        
+        user_input = input("Enter an IP address and network prefix (e.g., '192.168.1.0/24')or 'exit' to quit: ").strip().lower()   #  Allow user input
+        
 
-        if user_input.lower =="Exit":
+        #Allows the user to "exit" or "exit" or automatically exit if they enter anything else.
+        if user_input =="exit":
             print("Exiting the program.")
             break
-        process_ip_network(user_input)
-        get_ip_class(user_input)
+        if not user_input:
+            print("Enter valid IP address and network prefix (e.g., '192.168.1.0/24')or 'Exit' to quit: ")
+            continue
+    
+        #Try processing IP address
+        if process_ip_network(user_input):
+            try:
+                ip_class = get_ip_class(user_input)
+                print(f"IP Class: {ip_class}")
+                
 
+         #Handle invalid ip address  
+            except ValueError as e:
+                print(f"Error: {e}")
+                
 if __name__ =="__main__":
         main()
            
